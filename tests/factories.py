182f1d92ccf8ac2469894a523c6f83e5619a81a2
ilyass <ilyassjaatit@gmail.com>
2021-10-15 16:35:06 +0200

    user model (#10)

--- tests/factories.py
+++ tests/factories.py
@@ -1,9 +1,36 @@
+from django.contrib.auth import get_user_model
 import factory
 from faker import Faker
 from apps.products.models import Product, Category, Tag
 from apps.prestashop.models import PrestashopSynchronizer
 
 fake = Faker()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Faker("user_name")
+    email = factory.Faker("email")
+    first_name = factory.Faker("name")
+
+    @factory.post_generation
+    def password(self, create, extracted, **kwargs):
+        password = (
+            extracted
+            if extracted
+            else factory.Faker(
+                "password",
+                length=42,
+                special_chars=True,
+                digits=True,
+                upper_case=True,
+                lower_case=True,
+            ).evaluate(None, None, extra={"locale": None})
+        )
+        self.set_password(password)
+
+    class Meta:
+        model = get_user_model()
+        django_get_or_create = ["username"]
 
 
 class ProductFactory(factory.django.DjangoModelFactory):
