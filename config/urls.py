2daf30b8b41b177209350d3ea3c7d6514a07b47f
Ilyass Jaatit <ilyassjaatit@gmail.com>
2021-10-22 22:07:59 +0200

    add media config Django

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -24,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("config.api_router")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
 urlpatterns += [
