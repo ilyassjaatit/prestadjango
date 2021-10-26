1d949f1d7084c11d1891a2d2b0d77007d9b9f204
Ilyass Jaatit <ilyassjaatit@gmail.com>
2021-10-26 23:18:02 +0200

    basic connector to prestashop

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 
 class Customer(models.Model):
-    first_name = models.CharField(_('first name'), max_length=150, blank=True)
-    last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    email = models.EmailField(_('email address'), blank=True)
+    first_name = models.CharField(_('first name'), max_length=150)
+    last_name = models.CharField(_('last name'), max_length=150)
+    email = models.EmailField(_('email address'), unique=True)
+    active = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
--- apps/prestashop/admin.py
+++ apps/prestashop/admin.py
@@ -3,7 +3,7 @@ from apps.prestashop.models import PrestashopSynchronizer as PrestaSync
 
 
 class PrestashopSynchronizerAdmin(admin.ModelAdmin):
-    list_display = ('pk', "__str__", 'entity_id', 'status',)
+    list_display = ('pk', "__str__", "entity_type", 'entity_id', 'status',)
 
 
 admin.site.register(PrestaSync, PrestashopSynchronizerAdmin)
